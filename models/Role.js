19445b346992855805870b0bd5152a642b8cf5a8
Desarrollo Investigación Sanidad <desarrolloinvestigasanidad@gmail.com>
2025-03-11 07:13:48 +0100

    user role update

--- models/Role.js
+++ models/Role.js
@@ -3,9 +3,10 @@ const sequelize = require("../config/database");
 
 const Role = sequelize.define("Role", {
     id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
         primaryKey: true,
+        allowNull: false,
     },
     name: { type: DataTypes.STRING, allowNull: false, unique: true },
     description: { type: DataTypes.STRING, allowNull: true },

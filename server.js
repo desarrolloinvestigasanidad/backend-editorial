0540bcaff2ae4d984425c4fae53e8ab64675a4aa
Desarrollo Investigación Sanidad <desarrolloinvestigasanidad@gmail.com>
2025-03-13 05:31:12 +0100

    check endpoint

--- server.js
+++ server.js
@@ -31,6 +31,11 @@ app.use("/api/invoices", require("./routes/invoices"));
 app.use("/api/certificates", require("./routes/certificates"));
 app.use("/api/panels", require("./routes/panels"));
 
+//Endpoint de Check
+app.get("/", (req, res) => {
+    res.send("La aplicación está corriendo correctamente.");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
 
